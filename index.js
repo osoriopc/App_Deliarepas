90a7bd0f756231f303c51e50dd6013c5d1b0918f
osoriopc <osoriopc_1984@hotmail.com>
2023-04-25 09:27:16 -0500

    Se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import {} from "firebase/auth";
+import {} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBrHjb2ZhegNWitCwstr0dYRbKivRwYEk",
+  authDomain: "appcuadre.firebaseapp.com",
+  projectId: "appcuadre",
+  storageBucket: "appcuadre.appspot.com",
+  messagingSenderId: "1078284948935",
+  appId: "1:1078284948935:web:7de412df76558a17b9f5e2",
+};
+
+const app = initializeApp(firebaseConfig);
